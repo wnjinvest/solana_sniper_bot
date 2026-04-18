c23fd1e83d26fe0b5e58235fcdba3d842396ff00
champ-ieter <wnj.invest@gmail.com>
2026-04-18 18:30:04 +0200

    chore: update bot/jest.config.js

--- bot/jest.config.js
+++ bot/jest.config.js
@@ -1,13 +1,10 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset:          'ts-jest',
   testEnvironment: 'node',
   roots:           ['<rootDir>/src'],
   testMatch:       ['**/__tests__/**/*.test.ts'],
   testTimeout:     15_000,
-  globals: {
-    'ts-jest': {
-      tsconfig: { strict: false },
-    },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { strict: false } }],
   },
 };
